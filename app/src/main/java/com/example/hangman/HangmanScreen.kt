1e73dffe8ecb623b19235d5026ba2dc39d2330d9
LeraSher <LeraSher>
2026-09-16 14:09:03 +0100

    Screen updated

--- app/src/main/java/com/example/hangman/HangmanScreen.kt
+++ app/src/main/java/com/example/hangman/HangmanScreen.kt
@@ -3,7 +3,9 @@ package com.example.hangman
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,5 +29,37 @@ fun HangmanScreen(viewModel: HangmanViewModel = viewModel()) {
                     "🖤".repeat(viewModel.maxLives - viewModel.getLivesRemaining()),
             modifier = Modifier.padding(top = 16.dp)
         )
+
+        if (viewModel.isGameOver()) {
+            Text(
+                text = if (viewModel.isWon()) "You won!" else "You lost! The word was: ${viewModel.targetWord}",
+                modifier = Modifier.padding(top = 24.dp)
+            )
+        } else {
+            TextField(
+                value = letterInput,
+                onValueChange = {newValue ->
+                    if (newValue.length <= 1) {
+                        if (newValue.isEmpty() || newValue[0].isLetter()) {
+                            letterInput = newValue
+                        }
+                    }
+                },
+                singleLine = true,
+                modifier = Modifier.padding(top = 24.dp)
+            )
+            Button(
+                onClick = {
+                    val letter = letterInput.firstOrNull()
+                    if (letter != null) {
+                        viewModel.guessLetter(letter)
+                    }
+                    letterInput = ""
+                },
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text("Submit")
+            }
+        }
     }
 }
